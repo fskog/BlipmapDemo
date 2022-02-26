d1affa08cfd2eebbbc24a57772201a1aa3aed6a0
fskog <43705086+fskog@users.noreply.github.com>
2022-02-26 22:46:33 +0100

    Add project files.

---
+++ wwwroot/js/BlipmapVisualizerJs.js
@@ -0,0 +1,233 @@
+﻿console.log("BlipmapVisualizerJs");
+
+var show = false;
+var currentP5;
+var selectedAnimation;
+
+export function createVisualizer(animationId) {
+    console.log("Creating Visualizer with animation " + animationId + ".");
+    const animations = [lightsBoard, animation0, animationA];
+
+    window.document.getElementById('blipmapvisualizer').innerHTML = '';
+
+    selectedAnimation = animations.find(x => x.id === animationId) || animations[0];
+
+    show = true;
+    if (show) {
+        console.log("Importing p5js module.");
+        import('../lib/p5.min');
+        console.log("Creating canvas.");
+
+        currentP5 = new p5(sketch);
+    }
+}
+
+
+export function setGridSize(sizes) {
+    console.log("Setting grid size: Rows = " + sizes.rows + ", Columns = " + sizes.columns + ".");
+    selectedAnimation.options.lightRows = sizes.rows;
+    selectedAnimation.options.lightColumns = sizes.columns;
+    createVisualizer(selectedAnimation.id);
+};
+
+export function setActive(isActive) {
+    console.log("Setting active = " + isActive);
+    selectedAnimation.state.isActive = isActive;
+};
+
+export function setFramerate(framerate) {
+    console.log("Setting framerate to " + framerate);
+    selectedAnimation.state.framerate = framerate;
+    currentP5.frameRate(selectedAnimation.state.framerate);
+};
+
+export function setAnimationActive(isActive) {
+    console.log("Setting animation active = " + isActive);
+    selectedAnimation.state.isAnimationActive = isActive;
+};
+
+export function setColorShiftRate(rate) {
+    console.log("Setting color shift rate to " + rate);
+    selectedAnimation.state.colorShiftRate = rate;
+};
+
+
+let sketch = function (p) {
+    console.log("Intializing new sketch...");
+    console.log({ selectedAnimation });
+
+    p.setup = () => selectedAnimation.setup(p, 'blipmapvisualizer');
+
+    p.draw = () => selectedAnimation.draw(p);
+
+};
+
+
+//const lights = {
+//    id: "DefaultCircle",
+//    name: "Default Circle",
+//    state: {
+//        bg: 0,
+//    },
+//    setup: function (p) {
+//        console.log("Setup called.");
+//        var canvas = p.createCanvas(400, 400);
+//        canvas.parent('blipmapvisualizer');
+//    },
+//    draw: function (p) {
+//        p.background(this.state.bg);
+//        p.fill(255);
+//        p.circle(50, 50, 50);
+//    }
+//}
+
+
+
+const animation0 = {
+    id: "DefaultCircle",
+    name: "Default Circle",
+    state: {
+        bg: 0,
+    },
+    setup: function (p) {
+        console.log("Setup called.");
+        var canvas = p.createCanvas(400, 400);
+        canvas.parent('blipmapvisualizer');
+    },
+    draw: function (p) {
+        p.background(this.state.bg);
+        p.fill(255);
+        p.circle(50, 50, 50);
+    }
+}
+
+const animationA = {
+    id: "Bounce",
+    name: "Bounce",
+    state: {
+        rad: 30, // Width of the shape
+        xpos: 0,
+        ypos: 0, // Starting position of shape
+        xspeed: 2.8, // Speed of the shape
+        yspeed: 2.2, // Speed of the shape
+        xdirection: 1, // Left or Right
+        ydirection: 1, // Top to Bottom
+    },
+    setup: function (p, elementId) {
+        console.log("Setup called for animationA");
+        var canvas = p.createCanvas(400, 400);
+        canvas.parent(elementId);
+        p.noStroke();
+        p.frameRate(30);
+        p.ellipseMode(p.RADIUS);
+        // Set the starting position of the shape
+        this.state.xpos = p.width / 2;
+        this.state.ypos = p.height / 2;
+        console.log("Setup complete.");
+
+    },
+    draw: function (p) {
+        p.clear();
+        p.background(102);
+
+        // Update the position of the shape
+        this.state.xpos = this.state.xpos + this.state.xspeed * this.state.xdirection;
+        this.state.ypos = this.state.ypos + this.state.yspeed * this.state.ydirection;
+
+        // Test to see if the shape exceeds the boundaries of the screen
+        // If it does, reverse its direction by multiplying by -1
+        if (this.state.xpos > p.width - this.state.rad || this.state.xpos < this.state.rad) {
+            this.state.xdirection *= -1;
+        }
+        if (this.state.ypos > this.state.height - this.state.rad || this.state.ypos < this.state.rad) {
+            this.state.ydirection *= -1;
+        }
+
+        // Draw the shape
+        p.ellipse(this.state.xpos, this.state.ypos, this.state.rad, this.state.rad);
+    }
+}
+
+
+const lightsBoard = {
+    id: "Lights",
+    name: "Lights",
+    state: {
+        animationId: "lights",
+        lights: [],
+        isActive: true,
+        isAnimationActive: true,
+        brightness: 50,
+        colorShiftRate: 5,
+        bpm: 120,
+        framerate: 5,
+    },
+    options: {
+        tile: {
+            width: 10,
+            height: 10,
+            centerWidthOffset: 5,
+            centerHeightOffset: 5,
+        },
+        lightSize: 20,
+        lightRows: 3,
+        lightColumns: 3,
+    }
+    ,
+    setup: function (p, elementId) {
+        console.log("Setup called for Lights board");
+        var canvas = p.createCanvas(400, 400);
+        canvas.parent(elementId);
+        //p.noStroke();
+        p.frameRate(5);
+        //p.ellipseMode(p.RADIUS);
+        // Set the starting position of the shape
+        console.log("Setup complete.");
+
+        const rows = this.options.lightRows;
+        const columns = this.options.lightColumns;
+
+        this.options.tile.width = p.width / columns;
+        this.options.tile.height = p.height / rows;
+        this.options.tile.centerWidthOffset = this.options.tile.width / 2;
+        this.options.tile.centerHeightOffset = this.options.tile.height / 2;
+
+        for (var row = 0; row < rows; row++) {
+            var lightRow = [];
+            for (var column = 0; column < columns; column++) {
+                lightRow.push({
+                    isActive: true,
+                    brightness: 50,
+                    bpm: 120,
+                    color: {
+                        r: 100,
+                        g: 200,
+                        b: 50
+                    }
+                });
+            }
+            this.state.lights.push(lightRow);
+        }
+    },
+    draw: function (p) {
+        p.clear();
+        p.background(0);
+        if (!this.state.isActive) return;
+        p.fill(230);
+        this.state.lights.map((row, rowIndex) => {
+            row.map((light, lightIndex) => {
+                p.fill(light.color.r, light.color.g, light.color.b);
+                p.circle((this.options.tile.width * lightIndex) + this.options.tile.centerWidthOffset, (this.options.tile.height * rowIndex) + this.options.tile.centerHeightOffset, this.options.lightSize);
+
+                if (this.state.isAnimationActive) {
+                    light.color.r = light.color.r + (this.state.colorShiftRate * 8 * lightIndex) + (2 * rowIndex);
+                    if (light.color.r > 250) light.color.r = 0;
+                    light.color.g = light.color.g + (this.state.colorShiftRate * 5 * lightIndex) + (5 * rowIndex);
+                    if (light.color.g > 250) light.color.g = 0;
+                    light.color.b = light.color.b + (this.state.colorShiftRate * 2 * lightIndex) + (8 * rowIndex);
+                    if (light.color.b > 250) light.color.b = 0;
+                }
+            });
+        });
+    }
+}
---
+++ wwwroot/service-worker-assets.js
@@ -0,0 +1,285 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-8ZRc1sGeVrPBx4lD717BgRaQekyh78QKV9SKsdt638U=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-Jtxf9L+5ITKRc1gIRl4VbUpGkRNfOBXjYTdhJD4facM=",
+      "url": "favicon.ico"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-vMS7L5NNTLqpCq7SoPcQZyLLwtwKA4EO\/uJquTx8LII=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-dX9zRBcW7qg\/d8kiISSvgN5lj7RmP62pO+SPR59AQPg=",
+      "url": "js\/Animations\/BouncyBubbles.js"
+    },
+    {
+      "hash": "sha256-C7xjSWw3cnugk\/zUk5bWvs\/koJ+PHXgeuILNriTtSRs=",
+      "url": "js\/BlipmapVisualizerJs.js"
+    },
+    {
+      "hash": "sha256-OvAeyGVU63vWfyy20ZlStYy02ajmijO7Kf7qsMshdKU=",
+      "url": "lib\/p5.min.js"
+    },
+    {
+      "hash": "sha256-AxEWw5FRiCt8kW10snfMKV2JHkQ6aN8MGS6SHeYE83s=",
+      "url": "manifest.json"
+    },
+    {
+      "hash": "sha256-yzFf+O\/mlH+Q9klUSqXP2kxGKOUFLPxaww8da8fKhGU=",
+      "url": "sample-data\/weather.json"
+    },
+    {
+      "hash": "sha256-pD9dkEr0gbzoZKvepQmzbYKQYka7z+K9H2I+m5Inpxc=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-LFLUblpjDi9ERBHGTspEaG6Ps\/WiyJv82\/xaUPvkXZ8=",
+      "url": "_framework\/dotnet.timezones.blat"
+    },
+    {
+      "hash": "sha256-Zuq0dWAsBm6\/2lSOsz7+H9PvFaRn61KIXHMMwXDfvyE=",
+      "url": "_framework\/icudt.dat"
+    },
+    {
+      "hash": "sha256-WPyI4hWDPnOw62Nr27FkzGjdbucZnQD+Ph+GOPhAedw=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-4RwaPx87Z4dvn77ie\/ro3\/QzyS+\/gGmO3Y\/0CSAXw4k=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-OxylFgLJlFqixsj+nLxYVsv5iZLvfIKMpLf9hrWaChA=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-t6ZeEOQ4AbDfDm9zDCYURV0VtNGMxa4zxV0VFVJHiFc=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-DU1dEWvecj4AtybZ\/AtLOBtjog5OR1wvx82GEla3qoY=",
+      "url": "_framework\/Blipmap.dll"
+    },
+    {
+      "hash": "sha256-eD8rgfrAUmnsK+z0OyDK5GDlvhJcaPT\/jXEhCYrrDQc=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.dll"
+    },
+    {
+      "hash": "sha256-LqeJ8RNmZT6bfAdjq0tuKRjrzgBcMTplqxln2KXiQbM=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Forms.dll"
+    },
+    {
+      "hash": "sha256-tnq8SNBGoct\/gdjs5XmG5CLXg8bkqDutn\/bpBLGTjec=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.dll"
+    },
+    {
+      "hash": "sha256-yUU\/mWWa82QWVsGQj6w\/j4ja+GJcNtYn9qS4hbsDCPU=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-zhFtW4kE16TdrpPne9I1Cu9o0KYzrS50VE9MVSifJg4=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-QPR+1IXNcA28b\/gXw3uZ8aRigHIZUJ1KWSH02kkCg7Q=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.dll"
+    },
+    {
+      "hash": "sha256-fF3xaP+hT6R3GYUctLIY1ixaXQ8mrHzwWHHf2dKfueU=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.dll"
+    },
+    {
+      "hash": "sha256-+kKbwtKa42dsdbvl8ZSjUr6x6PzwfxS1F6WqivrbagA=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-pYDNELbgbO60zgvgH5SP8004wkA0+o\/KlQxFXl\/uS2A=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.dll"
+    },
+    {
+      "hash": "sha256-kf3HUdScrQ5nL65UDxYlJFd\/T8L6DhfmXHEbgYzDzLg=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.dll"
+    },
+    {
+      "hash": "sha256-aj8Tk1BLrOE4y41oBvw\/VGDdmvACVEpGmiEDNrA7Hco=",
+      "url": "_framework\/Microsoft.Extensions.Logging.dll"
+    },
+    {
+      "hash": "sha256-1tB1TdOK1QaMSk7+PPe5sikuuMC9hs8bkujWqFMdAx4=",
+      "url": "_framework\/Microsoft.Extensions.Options.dll"
+    },
+    {
+      "hash": "sha256-YColTWA58whC+8aBjv1O7HgsyYrbHTba601EkNDWAlo=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.dll"
+    },
+    {
+      "hash": "sha256-NWG3HvW5ZsOkwHnSoBU60WYVIRWfVGevxM2aFfLyfnA=",
+      "url": "_framework\/Microsoft.JSInterop.dll"
+    },
+    {
+      "hash": "sha256-+vVdIj7neJ+ihLFR+qU3xbNhxbIyjk4IHvJiJ3reJOs=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.dll"
+    },
+    {
+      "hash": "sha256-OeRDpGisaf5UJQm2BbRRNjpK7mHJ4FCPH6MPL+0IF+k=",
+      "url": "_framework\/MudBlazor.dll"
+    },
+    {
+      "hash": "sha256-JSaeS9VZMhq13n+Bq2qVgoT0sXZZS8E\/G4NamCV6o+o=",
+      "url": "_framework\/MudBlazor.ThemeManager.dll"
+    },
+    {
+      "hash": "sha256-SedNijYWJUN4zKkO8jWQ9De+fgwyj3FlmrMZ27a8W9Q=",
+      "url": "_framework\/System.Collections.Concurrent.dll"
+    },
+    {
+      "hash": "sha256-k95VQ10ijm7aEXwpiTgtaAKliV1mrK0NQ+KkrsZCJj0=",
+      "url": "_framework\/System.Collections.dll"
+    },
+    {
+      "hash": "sha256-AsYn8PuYclw1S\/VODQ8wObaH\/sGK9FFOMnixXTMsd9o=",
+      "url": "_framework\/System.ComponentModel.Annotations.dll"
+    },
+    {
+      "hash": "sha256-aOOfj6TSxwgorg3vhbjRXDYUmNHCm9FQc\/Fo2O2bsXo=",
+      "url": "_framework\/System.ComponentModel.dll"
+    },
+    {
+      "hash": "sha256-3AK7SkELQXrR689ajC+xiSBPawT3Jizsj9XZD\/v9cpw=",
+      "url": "_framework\/System.ComponentModel.Primitives.dll"
+    },
+    {
+      "hash": "sha256-rUYhs\/FtRBBbaXTyIGAHuE6D0Lg69VFHu2v7qfUGIM0=",
+      "url": "_framework\/System.ComponentModel.TypeConverter.dll"
+    },
+    {
+      "hash": "sha256-xJM2gxqcg+iZAmPft0Gl5LOiQtOYL2K7dZdwgL0iYBg=",
+      "url": "_framework\/System.Console.dll"
+    },
+    {
+      "hash": "sha256-\/HCI1og3hYP1S585JHVgHziVRiM3vE1z\/ehC64E0G28=",
+      "url": "_framework\/System.dll"
+    },
+    {
+      "hash": "sha256-\/zcpnBnkPLPKNq1uWMMa8uYqYFQ3JP7CMMKYp0TE1bQ=",
+      "url": "_framework\/System.Drawing.dll"
+    },
+    {
+      "hash": "sha256-EX3S5k06ch4ntSYRNEaQB1raMyxDrtyzp3HKy7S32E4=",
+      "url": "_framework\/System.Drawing.Primitives.dll"
+    },
+    {
+      "hash": "sha256-tGBUiLk5TDRg3nTydW1NnYOjxSzIDC46jPCvSB+aL2A=",
+      "url": "_framework\/System.Linq.dll"
+    },
+    {
+      "hash": "sha256-Gz2MypbelghJo\/qRgSaT6TcYi9o8zNpKgHHbna9QbdU=",
+      "url": "_framework\/System.Linq.Expressions.dll"
+    },
+    {
+      "hash": "sha256-uJvgoRBS4ciOCYTuFdqXMOrh9So0aev+fGnVJ+AY1Yw=",
+      "url": "_framework\/System.Linq.Queryable.dll"
+    },
+    {
+      "hash": "sha256-4GMfOCDp79eiWhqhJsBp3JSMdcgfh2HXyme\/k2xlz+E=",
+      "url": "_framework\/System.Memory.dll"
+    },
+    {
+      "hash": "sha256-241ahl8cYP16xlK++cZkOskV3J9uR1kpj4I5UlT13os=",
+      "url": "_framework\/System.Net.Http.dll"
+    },
+    {
+      "hash": "sha256-EUEuQbPJS9ZZoCxl4MYUgqSljPoj8N\/LYl5cSF1rpXY=",
+      "url": "_framework\/System.Net.Http.Json.dll"
+    },
+    {
+      "hash": "sha256-I3OB0jKCJiH8os3YIO0C9Ii4QuzoiZqBlJmrIg10\/yc=",
+      "url": "_framework\/System.Net.Primitives.dll"
+    },
+    {
+      "hash": "sha256-cRHNIOBjIiu0AXqQD9GqF1\/OrmkCBHToHfNThb\/fO5I=",
+      "url": "_framework\/System.ObjectModel.dll"
+    },
+    {
+      "hash": "sha256-uNHPvF0miU3ddmgbwpybhh4DdU9YT6YxXi7Exu6gWSM=",
+      "url": "_framework\/System.Private.CoreLib.dll"
+    },
+    {
+      "hash": "sha256-XcXAIGsVgRMMs+GvE5ASWgWDz4L62CciRfja8OyUK2c=",
+      "url": "_framework\/System.Private.Runtime.InteropServices.JavaScript.dll"
+    },
+    {
+      "hash": "sha256-ytGBzhB5Kom4hnZNNpzUnBqM3IpRtZ0G61RjEFZJM7g=",
+      "url": "_framework\/System.Private.Uri.dll"
+    },
+    {
+      "hash": "sha256-c7WHUSCkNKjzWZaed95byCaGHzxpzix+pBePG1\/ZpD0=",
+      "url": "_framework\/System.Runtime.CompilerServices.Unsafe.dll"
+    },
+    {
+      "hash": "sha256-h1jjT2WNnktS6wBsk2A9eZW434Wfc3pPs9wEaUSNDDY=",
+      "url": "_framework\/System.Runtime.dll"
+    },
+    {
+      "hash": "sha256-dL1BH4vHYPHBJek9CgAMUIONjU9WheJi5AnNJ742sZU=",
+      "url": "_framework\/System.Runtime.InteropServices.RuntimeInformation.dll"
+    },
+    {
+      "hash": "sha256-YIl5pM7vMnQ6HceW9+FVv7kb+Zt2X7SYhBVEiK7FvQo=",
+      "url": "_framework\/System.Runtime.Numerics.dll"
+    },
+    {
+      "hash": "sha256-K2jhH+ydojZVrpuWeSYdPuqYaJbH7fYivvEb06audKE=",
+      "url": "_framework\/System.Text.Encodings.Web.dll"
+    },
+    {
+      "hash": "sha256-sAYBDdbHk\/kX8vcl3yaxpmoOfsTt+osJHt2xzkW1iOA=",
+      "url": "_framework\/System.Text.Json.dll"
+    },
+    {
+      "hash": "sha256-ZewDlFFxZV99VILhT1bmQpmnCUgDZ\/UQI1F\/bLWJJco=",
+      "url": "_framework\/System.Text.RegularExpressions.dll"
+    },
+    {
+      "hash": "sha256-Z7QUCrylFPY16CL\/78\/2iWHjEC1czybD7jwVA9MiP0I=",
+      "url": "_framework\/System.Threading.dll"
+    },
+    {
+      "hash": "sha256-TeNcsIyETwl5b3ethx2YVd\/\/iMCfdXUfYdPF26fj3ns=",
+      "url": "_framework\/dotnet.6.0.2-mauipre.1.22102.15.gfl5z1gmge.js"
+    },
+    {
+      "hash": "sha256-MIKgvaES99UDqFIUBlwLN5iff+cgpZeL2+uilXacUU8=",
+      "url": "_framework\/dotnet.wasm"
+    },
+    {
+      "hash": "sha256-yTIViq6trxD\/zq4l2rr8ROdCmpsT8cf4GR1ziInu8H0=",
+      "url": "Blipmap.styles.css"
+    },
+    {
+      "hash": "sha256-hRoYtyCAXmhGOVamZkUrYeETqCSL4MhLtRFKSAU8Rg0=",
+      "url": "_content\/MudBlazor.ThemeManager\/MudBlazorThemeManager.css"
+    },
+    {
+      "hash": "sha256-5QWMVgffA5X+tVR4ZNHwRgnoxK9EH8DrdScCAZTU9kk=",
+      "url": "_content\/MudBlazor\/MudBlazor.min.css"
+    },
+    {
+      "hash": "sha256-VW0Nq6nbJe5VWIcYwqstEEvf5mvXF3d0MzbIMNXjfI0=",
+      "url": "_content\/MudBlazor\/MudBlazor.min.js"
+    }
+  ],
+  "version": "i6qwhrd9"
+};
